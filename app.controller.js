433f24d2a2b9ba7c4a8e307eaaaceb51be2262a6
bobmatyas <ballium@protonmail.com>
2019-04-05 16:27:34 -0500

    setup empty functions in controller for part 2 of lab

--- app.controller.js
+++ app.controller.js
@@ -17,6 +17,23 @@ function TodoController($scope) {
     completed: false,
     },
   ];
+
+  function addTask() {
+    // empty add task function
+    // this function will add a task to the to-do list based on the add task box
+  }
+
+  function removeTask() {
+    // empty remove task function
+    // this function will remove a task from the to-do list based on clicking the "x" 
+  }
+
+  function completeTask() {
+    // empty complete task function 
+    // this functin sets the tasks completed property to true
+  }
+
+
 }
 
 // array of objects with name, relation, age [{}]   variable familyMembers
